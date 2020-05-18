049012d59efb1ed29a8eb52dbf8bef13f3d27bb1
Tommy <taguilu01@gmail.com>
2020-05-17 18:32:09 -0700

    my commit

--- mysite/blogging/urls.py
+++ mysite/blogging/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from blogging.views import list_view
+from blogging.views import list_view, detail_view
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('',
-         list_view,  # <-- Change this value from stub_view
-         name="blog_index"),
+    path('', list_view,name="blog_index"),
+    path('posts/<int:post_id>/', detail_view, name="blog_detail"),
+    path('login/', LoginView.as_view(template_name='login.html'), name="login"),
+    path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
 ]
